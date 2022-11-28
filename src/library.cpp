e8ef5de6a7f6f380f3038d66fdf86b32c6b013c2
Mateusz Paszkowski <mmpaszkowski@gmail.com>
2022-11-28 22:47:49 +0100

    Refactor

--- src/library.cpp
+++ src/library.cpp
@@ -1,9 +1,13 @@
 #include "../include/cpp_static_lib_template/library.h"
 
-#include <iostream>
+#include <fmt/color.h>
+#include <config.h>
 
 void hello() {
-    std::cout << "Hello, World!" << std::endl;
+    fmt::print(fg(fmt::color::white), project_name);
+    fmt::print(fg(fmt::color::green), " v");
+    fmt::print(fg(fmt::color::green), project_version);
+    fmt::print(fg(fmt::color::green), "\n");
 }
 
 double multiply(double a, double b) {
